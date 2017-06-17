766adeca4d6488cb125d43dfe91ad4fc74c55f49
BDCraven <bencraven@hotmail.com>
2017-06-17 17:07:27 +0100

    Updated Ch8 ex1.rb with simpler solution.

--- chap08/ex1.rb
+++ chap08/ex1.rb
@@ -1,10 +1,25 @@
+#puts "Please enter some words for me to sort. Press enter after each word and when you're done press enter on an empty line."
+#word = "x"
+#words = []
+#while word != ""
+#  word = gets.chomp
+#  if word != ""
+#  words << word
+#  end
+#end
+
+#if !words.empty?
+#  puts "Here are your words sorted alphabetically: #{words.sort.join(", ")}"
+#end
+
 puts "Please enter some words for me to sort. Press enter after each word and when you're done press enter on an empty line."
-word = "x"
 words = []
-while word != ""
+while true
   word = gets.chomp
   if word != ""
-  words << word
+    words << word
+  else
+    break
   end
 end
 
