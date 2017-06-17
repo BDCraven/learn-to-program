cc5fda80d7afa160fd78ced2d317d9cb94ca1bab
BDCraven <bencraven@hotmail.com>
2017-06-17 17:09:39 +0100

    Updated Ch10 eg2 with more comments.

--- chap10/eg2.rb
+++ chap10/eg2.rb
@@ -10,22 +10,22 @@ def factorial(num)
   end
 end
 
+puts factorial(4)
+# 4 * factorial(3)evaluates to a call to factorial(num) with 3 as num -1
+# 3 * factorial(2)
+# 2 * factorial(1)
+# num then equals 1 so returns 1 exiting the method and stopping the
+# recursive loop is equivalent to telling the computer to do 4 * 3 * 2 * 1
+# = 24
+puts factorial(5) # should equal 120
+# 5 * factorial(5-1)
+# 4 * factorial(3)
+# 3 * factorial(2)
+# 2 * factorial(1)
+# num is now 1 so we return 1 (append 1 to our algorithm) and exit the method
+# so 5 * 4 * 3 * 2 * 1 = 120
 puts factorial(3)
-# 4 * factorial(3)evaluates to a call to factorial(num) with 3 as num
-# so it is saying 3 the return value of the method until  
-
-3 * factorial(2) returns 6 but calls the method again
-# 2 * factorial(1) returns 1
-# num then equals 1 so returns 1
-# 4 * 6 * 1 = 24
-# puts factorial(5)
-# 5 * 4
-# 4 * 3 returns 12
-# 3 * 2 returns 6
-# 2 * 1 returns
-# puts factorial(3)
 # puts factorial(30)
 # the factorial of an integer is the product
 # of all the integers from itself down to 1
 # e.g. 3 x 2 x 1 = 6
-#
